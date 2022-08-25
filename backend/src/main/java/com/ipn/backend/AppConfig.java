1465a9436037a56aaed0ce96c5879c5a292097c6
axelcoevas <axelcoevas@gmail.com>
2022-08-25 16:06:05 -0500

    Inicializando el proyecto para la API

---
+++ backend/src/main/java/com/ipn/backend/AppConfig.java
@@ -0,0 +1,8 @@
+package com.ipn.backend;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    
+}
---
+++ backend/src/main/java/com/ipn/backend/SpringAndReactApplication.java
@@ -0,0 +1,20 @@
+package com.ipn.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author F18458
+ */
+@SpringBootApplication
+public class SpringAndReactApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndReactApplication.class, args);
+    }
+    
+}
